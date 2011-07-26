b0c9c7f1d1d172d77b8f3f40c065ca7da654c580
Dominic Tarr <dominic.tarr@gmail.com>
2011-07-26 21:06:07 +1000

    basic test running command

--- cmd.js
+++ cmd.js
@@ -1,22 +1,69 @@
 var Reporter = require('test-report')
 
+//
+// usage 
+// [cmd] test/*.js --file [write report to file] 
 
+function run (file, loader, adapter, reporter) {
+  var shutdown = function () {}
+    , failed = false
 
-function run (files, adapter, fname) {
-  var reporter = new Reporter(fname)
-    , shutdown
-    
-  if ('string' === typeof files) files = [files]
-
-  try{
-    tests = require (files[0])
-  } catch (error) {
-//    failed = true
-    reporter.error(error)  
-  }
+    try{
+      tests = loader (file)
+    } catch (error) {
+      failed = true
+      reporter.error(error)
+    }
   
-  if (adapter)
-    shutdown = adapter.run(tests, reporter, function () {})
+    if (adapter && !failed)
+      shutdown = adapter.run(tests, reporter.subreport(files[0]), function () {})
+
+  return shutdown
+}
+
+function args (argv) {
+  var n
+    , o = {args: []}
+  while(argv.length) {
+    n = argv.shift()
+    if(n == '--reportFile')
+      o.reportFile = argv.shift()
+    else 
+      o.args.push(n)
+  }
+  return o
+}
+
+function go(adapter) {
+  //parse arguments, load files, run command
+  var opts = args(process.argv.slice(2))
+    , tests = opts.args
+    , reportFile = opts.reportFile 
+    , reporter = new Reporter(process.cwd())
+    , shutdowns = 
+  tests.map(function (file) {
+    return run(file, require, adapter, reporter.subreport(file))
+  })
+
+  process.on('SIGTSTP',function (){
+    reporter.error(new Error("recieved stop signal due to timeout"))
+    process.exit()
+  })
+
+  process.on('exit', function (){
+    //
+    // trust the adapter to catch any thing thrown during shutdown.
+    //
+    shutdowns.forEach(function(e){e()})
+    if(reportFile)
+      fs.writeFileSync(reportFile,reporter.report)
+    else
+      console.log(reporter.report)
+  })
 }
 
 exports.run = run
+exports.go = go
+exports.args = args
+
+if(!module.parent) go()
